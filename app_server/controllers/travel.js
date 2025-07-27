5be44a5d9ad90fcb9fe154d739941add61d1f3c2
Zinedine De Leon <zinedinedeleon.dev@gmail.com>
2025-07-27 17:21:13 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,12 @@
-/* GET travel view */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
+
+/* GET travel view */
 const { title } = require("process")
 
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Gateways' });
+    res.render('travel', { title: 'Travlr Gateways', trips });
     console.log(res);
 };
 
